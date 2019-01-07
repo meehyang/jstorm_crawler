03faf9f803f2caa2ade16895f313851d63e16d59
먕 <>
2019-01-07 21:32:57 +0900

    requests와 BeautifulSoup 를 사용해서 목록 페이지에서 각 상세 페이지로 가는 링크들을 리스트로 가져온다.

---
+++ index.py
@@ -0,0 +1,13 @@
+import sys
+import requests
+import json
+from bs4 import BeautifulSoup
+
+if __name__ == "__main__":
+    url = sys.argv[1]   # console에서 url 인자를 받아서 쓴다
+    req = requests.get(url)
+    html = req.text
+    soup = BeautifulSoup(html, 'html.parser')
+
+    links = soup.select('body > div.l-content > ul.c-cards.js-masonry-container.content-start-line > li > a')
+
