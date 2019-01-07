4209840a36c913bee693b8f2cc5c714aee3cdbc3
먕 <>
2019-01-07 21:38:11 +0900

    상세 페이지 BeautifulSoup로 파싱

--- index.py
+++ index.py
@@ -15,3 +15,8 @@ if __name__ == "__main__":
     for link in links:
         album_links.append(link.get('href'))
 
+    discograpy = {}  # 앨범 순서대로 0 : {상세}, 1: {상세} 형태로 저장할 수 있도록 딕셔너리 변수를 만든다.
+    for index, album_link in enumerate(album_links):
+        req = requests.get(album_link)
+        html = req.content          # req.text로 가져오니 문자가 깨지는 문제가 발생하여 대신 req.content 형태로 가져온다. http://ourcstory.tistory.com/78 참고
+        soup = BeautifulSoup(html, 'html.parser', from_encoding='utf-8')
